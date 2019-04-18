cfb4f00f6d6f8b131a69caca2aa21c3bd0ad141e
coco <liuxiaofeikeke@163.com>
2019-04-18 17:19:01 +0800

    修正表结构,精简静态文件

--- web/models.py
+++ web/models.py
@@ -1,133 +1,196 @@
 from django.db import models
 
 
-#部门
-departments_choices = (
-    ('develop',"开发"),
-    ('operation',"运维"),
-    ('test',"测试"),
-)
-
-#主机状态
-hosts_status_choices =(
-    ('online','在线'),
-    ('offline','离线'),
-    ('maintain','维修'),
-    ('stop_use','停用'),
-)
-
-#主机环境
-hosts_environment_choices = (
-    ('test','测试'),
-    ('developing','开发'),
-    ('pre_production ','预生产'),
-    ('production ','生产'),
-    )
-
-#项目状态
-team_status_choices = (
-    ('in_use','在用'),
-    ('off_line','下线'),
-)
-
-#发布状态
 issue_status_choices = (
-    ('wait_issue','等待发布'),
-    ('issueing','发布中'),
-    ('wait_test','等待测试'),
-    ('pass_test','测试通过'),
-    ('issue_success','发布成功'),
-    ('roll_back','回滚'),
-)
-
-#备份状态
-backups_status_choiecs = (
-    ('yes','已备份'),
-    ('no','未备份'),
-)
+        ('0', '等待发布'),
+        ('1', '发布中'),
+        ('2', '等待测试'),
+        ('3', '测试通过'),
+        ('4', '发布成功'),
+        ('5', '失败'),
+        ('6', '回滚失败'),
+        ('7', '回滚成功'),
+    )
 
 #用户表
 class User(models.Model):
-    username = models.CharField('用户名',max_length=15)
+    # 部门
+    departments_choices = (
+        ('develop', "开发"),
+        ('operation', "运维"),
+        ('test', "测试"),
+    )
+
+    username = models.CharField('用户名',max_length=150)
     password = models.CharField(max_length=18)
-    role = models.ManyToManyField("Role")
+    role = models.CharField(verbose_name='角色',choices=(('0','开发'),('1','测试'),('2','运维')),default='0',max_length=10)
     email = models.EmailField(max_length=254,unique=True)
     department = models.CharField('部门',choices=departments_choices,max_length=20)
-    create_time = models.DateTimeField('创建时间',max_length=50)
+    phone = models.CharField('手机号',max_length=11,blank=True,null=True)
+    is_admin = models.CharField('管理员',choices=(("0",'admin'),('1','普通')),max_length=10,default='1')
+    create_time = models.DateTimeField('创建时间',max_length=50,auto_now_add=True)
 
-#角色表
-class Role(models.Model):
-    name = models.CharField('角色名称',max_length=30)
+    class Meta:
+        ordering = ['-create_time']
+
+    def __str__(self):
+        return self.username
+
 
 #主机表
 class Host(models.Model):
-    name = models.CharField('主机名称',max_length=254)
-    ip = models.CharField('ip',max_length=16)
+
+    # 主机状态
+    hosts_status_choices = (
+        ('online', '在线'),
+        ('offline', '离线'),
+        ('maintain', '维修'),
+        ('stop_use', '停用'),
+    )
+
+    # 主机环境
+    hosts_environment_choices = (
+        ('test', '测试'),
+        ('developing', '开发'),
+        ('pre_production ', '预生产'),
+        ('production ', '生产'),
+    )
+    #主机类型
+    Type = (
+        ("0", "nginx"),
+        ("1", "redis"),
+        ("2", "db"),
+        ("3", "server"),
+    )
+
+    name = models.CharField('主机名称',max_length=254,unique=True)
+    ip = models.GenericIPAddressField('主机ip地址')
     version = models.CharField('版本',max_length=132)
-    ssh = models.CharField('ssh端口',max_length=16)
-    type = models.CharField('主机类型',max_length=12)
-    hosts_status = models.CharField('主机状态',choices=hosts_status_choices,max_length=64)
-    hosts_environment = models.CharField('主机环境',choices=hosts_environment_choices,max_length=64)
-    inits = models.ForeignKey('Init')
+    ssh = models.CharField('ssh端口',max_length=16,default=22)
+    type = models.CharField('主机类型',max_length=12,choices=Type,default='3')
+    hosts_status = models.CharField('主机状态',choices=hosts_status_choices,max_length=64,default='production')
+    hosts_environment = models.CharField('主机环境',choices=hosts_environment_choices,max_length=64,default='production')
+
+    def __str__(self):
+        return self.ip
 
 #项目表
 class TeamProjt(models.Model):
-    name = models.CharField('项目名称',max_length=32)
-    create_time = models.DateTimeField('项目创建时间',max_length=50)
-    '''多对多关系'''
-    principal = models.ManyToManyField('User',related_name="负责人",verbose_name='负责人')
-    tester = models.ManyToManyField('User',related_name="测试人",verbose_name='测试人')
-    developer = models.ManyToManyField('User',related_name="开发人",verbose_name='开发人')
-    server_host = models.ManyToManyField('Host',related_name="其他服务器",verbose_name='其他服务器')
-    nginx_host = models.ManyToManyField('Host',related_name="nginx服务器",verbose_name='nginx服务器')
-    develop_language = models.CharField('开发语言',max_length=20)
-    team_status = models.CharField(choices=team_status_choices,verbose_name='项目状态',max_length=64)
-    nginx_conf = models.CharField('nginx配置信息',max_length=254)
-    project_path = models.CharField(verbose_name='linux项目路径',max_length=60)
-    git_path = models.CharField(verbose_name='git地址',max_length=80)
-    note = models.CharField('备注',max_length=300)
+    # 项目状态
+    team_status_choices = (
+        ('0', '在用'),
+        ('1', '下线'),
+    )
+    #开发语言
+    Language = (
+        ("0", 'python'),
+        ("1", "java"),
+        ("2", 'go'),
+        ("3", "php"),
+        ("4", "html")
+    )
+
+    name = models.CharField('项目名称',max_length=32,unique=True)
+    create_time = models.DateTimeField('项目创建时间',max_length=50,auto_now_add=True )
+    boss = models.ManyToManyField('User',related_name="boss",verbose_name='负责人')
+    tester = models.ManyToManyField('User',related_name="tester",verbose_name='测试人')
+    developer = models.ManyToManyField('User',related_name="developer",verbose_name='开发人')
+    ops_user = models.ManyToManyField('User',related_name="ops_user",verbose_name='运维人')
+    server_host = models.ManyToManyField('Host',related_name="server_host",verbose_name='后端主机')  #后端主机
+    nginx_host = models.ManyToManyField('Host',related_name="nginx_host",verbose_name='nginx服务器')
+    develop_language = models.CharField('开发语言',max_length=20,choices=Language,default='0')
+    team_status = models.CharField(choices=team_status_choices,verbose_name='项目状态',default='0', max_length=2)
+    nginx_conf = models.CharField('nginx配置信息',max_length=254,null=True,blank=True)
+    project_path = models.CharField(verbose_name='linux项目路径',max_length=200)
+    git_path = models.CharField(verbose_name='git地址',max_length=200)
+    note = models.CharField('备注',max_length=300,null=True,blank=True)
+    domain = models.CharField('域名',max_length=100,null=True,blank=True)
+
+    class Meta:
+        ordering = ("-create_time",)
+    def __str__(self):
+        return self.name
 
 #命令表
 class Command(models.Model):
-    command_name = models.CharField('执行命令名称',max_length=120)
-    result = models.CharField('执行结果', max_length=120)
-    operator = models.ForeignKey('User',verbose_name='执行人')
-    host_list = models.ForeignKey('Host',verbose_name='主机列表')
+    command = models.CharField(verbose_name="命令", max_length=200)
+    result = models.CharField(verbose_name="结果", max_length=2000)
+    hosts_list = models.CharField(verbose_name="执行机器", max_length=20000)
+    user = models.ForeignKey('User', verbose_name='用户')
+    create_time = models.DateTimeField("创建时间", auto_now_add=True)
 
+    class Meta:
+        ordering = ['-create_time']
 
 #定时任务表
 class Cron(models.Model):
-    cron_name = models.CharField('任务名称',max_length=32)
-    time = models.DateTimeField('时间',max_length=30)
+    name = models.CharField('计划名称',max_length=32,unique=True,)
+    time = models.CharField('计划任务执行时间',max_length=32)
+    host_list = models.CharField('执行机器', max_length=200)
     job = models.CharField('任务',max_length=120)
-    note = models.CharField('备注',max_length=300)
-    linux_user = models.CharField('linux用户',max_length=16)
-    operator = models.ForeignKey('User',verbose_name='执行人')
+    note = models.CharField('备注',max_length=300,null=True,blank=True)
+    user = models.ForeignKey('User',verbose_name='创建者')
+    linux_user = models.CharField(verbose_name='执行人',null=True,blank=True,default='root',max_length=30)
+    create_time = models.DateTimeField('创建时间',auto_now_add=True)
+
+    class Meta:
+        ordering = ['-create_time']
+
+    def __str__(self):
+        return self.name
 
 #初始化表
 class Init(models.Model):
-    init_name = models.CharField('初始化名称',max_length=30)
-    system = models.CharField('系统名称',max_length=50)
-    init_function = models.CharField('初始化功能',max_length=200)
-    playbook = models.CharField('playbook',max_length=20)
-    init_time = models.DateTimeField('初始化时间',max_length=20)
+    name = models.CharField('名称',max_length=30,unique=True)
+    function = models.CharField('初始化功能',max_length=200,unique=True)
+    playbook = models.CharField('playbook路径',max_length=100)
+    create_time = models.DateTimeField('创建时间',auto_now_add=True)
+    create_user = models.ForeignKey('User',verbose_name='创建者')
+
+    class Meta:
+        ordering = ['-create_time']
+
+    def __str__(self):
+        return self.name
 
 #发布表
 class Issue(models.Model):
-    issuer = models.ForeignKey('User',verbose_name='发布人')
-    version_num = models.CharField('版本号',max_length=50)
-    issue_team = models.ForeignKey('TeamProjt',verbose_name='发布项目')
-    issue_status =models.CharField('发布状态',choices=issue_status_choices,max_length=64)
+
+    # 备份状态
+    backups_status_choiecs = (
+        ('0', '已备份'),
+        ('1', '未备份'),
+    )
+
+    team = models.ForeignKey('TeamProjt',verbose_name='发布项目')
+    user = models.ForeignKey('User',verbose_name='发布人')
+    type = models.CharField('发布类型',max_length=50,choices=(('0','文件'),('1','git')),default='0')
+    status =models.CharField('发布状态',choices=issue_status_choices,max_length=64,default='0')
     file_dir = models.CharField('部署发布系统',max_length=50)
-    backups = models.CharField('备份情况',choices=backups_status_choiecs,max_length=64)
-    backup_path = models.CharField('备份地址',max_length=30)
+    backup = models.CharField('备份情况',choices=backups_status_choiecs,max_length=12,default='0')
+    backup_path = models.CharField('备份文件地址',max_length=300,null=True,blank=True)
+    upload_path = models.CharField('上传文件地址',max_length=300,null=True,blank=True)
+    create_time = models.DateTimeField('创建时间', auto_now_add=True)
+
+    class Meta:
+        ordering = ['-create_time']
 
 #主机发布表
 class HostIssue(models.Model):
-    hosts = models.ForeignKey('Host',verbose_name='主机信息')
-    innues = models.ForeignKey('Issue',verbose_name='发布信息')
+    host = models.ForeignKey('Host',verbose_name='发布机器')
+    issue = models.ForeignKey('Issue',verbose_name='发布信息')
+    team = models.ForeignKey('TeamProjt',verbose_name='发布项目')
     time = models.DateTimeField('主机发布时间',max_length=30)
-    host_issue_status = models.CharField('主机发布状态',choices=issue_status_choices,max_length=64)
+    status = models.CharField('主机发布状态',choices=issue_status_choices,max_length=64,default='0',)
 
 
 
+#初始化日志表
+class InitLog(models.Model):
+    init = models.ForeignKey(Init, verbose_name="初始化功能")
+    hosts_list = models.ManyToManyField(Host, verbose_name="执行机器")
+    user = models.ForeignKey(User, verbose_name="创建者")
+    create_time = models.DateTimeField("创建时间", auto_now_add=True)
+
+    class Meta:
+        ordering = ('-create_time',)
